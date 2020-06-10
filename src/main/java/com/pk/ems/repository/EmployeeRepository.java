206c69346613f4839faf09e86eb6f9ac927dabbd
pikactct <kdas@gmail.com>
2020-06-10 20:15:57 +0530

    initail commit

---
+++ src/main/java/com/pk/ems/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.pk.ems.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pk.ems.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
